866379d788fd4f644d4ebdb5fde7dd548d024823
DanGitSu <sudaniel9@gmail.com>
2023-11-07 16:34:20 +1100

    creating movement scripts for tweening'

---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class PacStudentController : MonoBehaviour
+{
+    public Tweener tweener;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!tweener.TweenExists(transform))
+        {
+            Vector3 flipX = new Vector3(-1, 1, 1);
+            target = Vector3.Scale(target, flipX);
+            tweener.AddTween(transform, transform.position, target, duration / SpeedManager.SpeedModifier);
+        }*/
+    }
+}
+
