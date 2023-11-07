42c76eed821aac752143649109a14f7af1d96d41
DanGitSu <sudaniel9@gmail.com>
2023-11-07 17:57:06 +1100

    adding tweener

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -6,7 +6,8 @@ using static UnityEngine.GraphicsBuffer;
 public class PacStudentController : MonoBehaviour
 {
     public Tweener tweener;
-    private Vector3 movement;
+    private Vector3 lastInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,31 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lastInput = new Vector3(0,1,0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lastInput = new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            lastInput = new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lastInput = new Vector3(1, 0, 0);
+        }
+
+        if (!tweener.TweenExists(transform))
+        {
+            Vector3 flipX = new Vector3(-1, 1, 1);
+            target = Vector3.Scale(target, flipX);
+            tweener.AddTween(transform, transform.position, target, duration / SpeedManager.SpeedModifier);
+        }
+
+
         /*if (!tweener.TweenExists(transform))
         {
             Vector3 flipX = new Vector3(-1, 1, 1);
