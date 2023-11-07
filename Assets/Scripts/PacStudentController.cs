ec82424e0b0e91766c466af4aa262988ec0b4bcf
DanGitSu <sudaniel9@gmail.com>
2023-11-07 19:43:47 +1100

    map added

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -9,6 +9,26 @@ public class PacStudentController : MonoBehaviour
     private Vector3 lastInput;
     private float duration = 0.5f;
 
+    int[,] levelMap =
+            {
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+            {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+            {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+            {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+            {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+            {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+            {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+            {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+            {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+            {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+            };
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +55,7 @@ public class PacStudentController : MonoBehaviour
             lastInput = new Vector3(0.5f, 0, 0);
         }
 
+        if (transform.position == )
         if (!tweener.TweenExists(transform))
         {
             tweener.AddTween(transform, transform.position, transform.position + lastInput, duration);
